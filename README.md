7a3a8e85b4993352fd172ef87b527a914838c2cf
Mirela Stanescu <mirela.stanescu@pss.ro>
2023-01-15 22:50:27 +0200

    change readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ReactProject2
+
+Aplicatie web pentru magazin online.
+Permite vizualizarea produselor din magazin, gruparea lor pe categorii si adaugarea sau eliminarea lor in Shoping cart.
--- src/apis/firebase/firebase.js
+++ src/apis/firebase/firebase.js
@@ -1,6 +1,9 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 import firebaseConfig from '../../configs/firebase';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 firebase.initializeApp(firebaseConfig);
 const googleProvider = new firebase.auth.GoogleAuthProvider()
